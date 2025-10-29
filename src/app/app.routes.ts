9db5f4899bfe352e9cea95da29f4b35cb12e0d4f
JuanC1824 <juanjosecardenas142006@gmail.com>
2025-10-28 21:09:11 -0500

    nuevas Correcciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { CronometroComponent } from './cronometro/cronometro.component';
 
 export const routes: Routes = [
   { path: 'cronometro', component: CronometroComponent },
   { path: '', redirectTo: '/cronometro', pathMatch: 'full' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
